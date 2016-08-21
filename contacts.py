2ae77793c6ebba81bb50e896e54b2aee2d50ecdd
Enemyz0r <rares_taut@yahoo.com>
2016-08-21 12:04:54 +0300

    Update contacts.py

--- contacts.py
+++ contacts.py
@@ -1,18 +1,25 @@
+# -*- coding: utf-8 -*-
+# python 3.x
+# Create a csv file from dictionary
+# File format: name, phone number
+# Usage: dicttocsv.py
+# eg.(in bash): python3 dicttocsv.py
+######################################
+
 import csv
 
-Flag = True
+Flag = True # flag for program loop
 
 while Flag:
-    contact = input("Introduceti Numele si numarul de telefon: ")
-    dicktionary = dict(x.split() for x in contact.splitlines())
-    maiDoriti = input("Mai doriti sa introduceti numere? (y/n)")
+    contact = input("Please enter name and phone number (eg. John 555-789134): ")
+    dictionary = dict(x.split() for x in contact.splitlines())
+    choice = input("Would you like to further add numbers?  (y/n)")
     with open('contacts.csv', 'a') as csv_file:
             writer = csv.writer(csv_file)
-            for key, value in dicktionary.items():
+            for key, value in dictionary.items():
                 writer.writerow([key, value])
 
-    if maiDoriti == 'y':
+    if choice == 'y':
         Flag = True
     else:
         Flag = False
-
